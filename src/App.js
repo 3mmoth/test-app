1641d5eb2e4a062f0355b8d45f8ffd665eb67532
Linus <le222je@student.lnu.se>
2021-01-06 14:59:41 +0100

    final adjustment

--- src/App.js
+++ src/App.js
@@ -17,8 +17,8 @@ function App() {
 
       <Switch>
         <Route path="/test-app/" exact component= {withRouter(List)}/>
-        <Route path="/skattesats/:id" exact component= {withRouter(Skattesats)}/>
-        <Route path="/skattekraft/:id" exact component = {withRouter(Skattekraft)}/>
+        <Route path="/test-app/skattesats/:id" exact component= {withRouter(Skattesats)}/>
+        <Route path="/test-app/skattekraft/:id" exact component = {withRouter(Skattekraft)}/>
       </Switch>
 
       <Footer />
